2a70e091588eb07be36da5874c39e940563af574
Geeky Coder <toyibe25@gmail.com>
2022-10-08 12:06:59 +0100

    inital

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css';
-import './styles/fontawesome-free-6.1.1-web/css/all.css';
+// import './styles/fontawesome-free-6.1.1-web/css/all.css';
 import ScrollToTop from './Components/ScrollToTop';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
